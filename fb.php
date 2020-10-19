527b7485ba9293a7b33047cf489dd0de44009dd5
erik-g-z <72345570+erik-g-z@users.noreply.github.com>
2020-10-19 11:54:37 -0300

    Update fb.php

--- fb.php
+++ fb.php
@@ -7,9 +7,9 @@
     //instancia a classe Facebook
     $fb = new Facebook\Facebook([
         //define o app id
-        'app_id' => '1518508458539257',
+        'app_id' => '',
         //define o token
-        'app_secret' => 'e139e053d76a161796abac0d23962ffa',
+        'app_secret' => '',
         //define a versão
         'default_graph_version' => 'v8.0'
     ]);
